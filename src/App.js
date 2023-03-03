857be1d13bb8228afe69f92087999a773c1ae072
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2023-03-03 17:23:16 +0300

    Modal page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './scss/App.scss';
 import MainPage from "./shared/components/page/mainPage/mainPage";
 import Registration from "./shared/components/registration/registration";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-      <HashRouter>
-          <div className="App">
-              <div className="container">
-                  <Routes>
-                      <Route exact path="/" element={<MainPage />} />
-                      <Route exact path="/registration" element={<Registration />} />
-                  </Routes>
-              </div>
+      <div className="App">
+          <div className="container">
+              <Routes>
+                  <Route exact path="/" element={<MainPage />} />
+                  <Route exact path="/registration" element={<Registration />} />
+              </Routes>
           </div>
-      </HashRouter>
+      </div>
   );
 }
 
